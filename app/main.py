2e5049d66a70305135dcd4ad25518812c5117089
Volodymyr Yalechko <Volodymyr.I.Yalechko@lpnu.ua>
2026-03-29 20:48:14 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -8,18 +8,24 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    instances = [Person(p.get("name"), p.get("age")) for p in people]
+    Person.people.clear()
+
+    instances = [
+        Person(p.get("name"), p.get("age"))
+        for p in people
+    ]
 
     for person_dict in people:
         name = person_dict.get("name")
-        person_instance = Person.people[name]
+        person_instance = Person.people.get(name)
 
         wife_name = person_dict.get("wife")
         husband_name = person_dict.get("husband")
 
         if wife_name:
-            person_instance.wife = Person.people[wife_name]
+            person_instance.wife = Person.people.get(wife_name)
+
         if husband_name:
-            person_instance.husband = Person.people[husband_name]
+            person_instance.husband = Person.people.get(husband_name)
 
     return instances
